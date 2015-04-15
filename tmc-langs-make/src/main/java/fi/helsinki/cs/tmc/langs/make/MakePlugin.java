4e4976b6918df6f670ebac467df4a347c706a0f1
Tahantos <jokkend@gmail.comgit config --global user.email jokkend@gmail.com>
2015-04-15 18:24:07 +0300

    fixed issue with previous commit

--- tmc-langs-make/src/main/java/fi/helsinki/cs/tmc/langs/make/MakePlugin.java
+++ tmc-langs-make/src/main/java/fi/helsinki/cs/tmc/langs/make/MakePlugin.java
@@ -1,25 +1,31 @@
 package fi.helsinki.cs.tmc.langs.make;
 
-import fi.helsinki.cs.tmc.langs.AbstractLanguagePlugin;
-import fi.helsinki.cs.tmc.langs.ExerciseDesc;
-import fi.helsinki.cs.tmc.langs.RunResult;
+import fi.helsinki.cs.tmc.langs.*;
 import fi.helsinki.cs.tmc.langs.RunResult.Status;
 import fi.helsinki.cs.tmc.langs.utils.TestResultParser;
 import java.io.File;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
-public class MakePlugin extends AbstractLanguagePlugin{
+public class MakePlugin extends AbstractLanguagePlugin {
 
     @Override
     protected boolean isExerciseTypeCorrect(Path path) {
         return new File(path.toString() + File.separatorChar + "Makefile").exists();
     }
-    
+
     @Override
     public String getLanguageName() {
-        return "C/C++";
+        return "C/C++/Fortran";
     }
-    
-    
-    
+
+    @Override
+    public ExerciseDesc scanExercise(Path path, String exerciseName) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public RunResult runTests(Path path) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
